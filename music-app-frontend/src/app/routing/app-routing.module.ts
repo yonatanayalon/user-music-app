b2aa6ea2a745a6d7c18fa7fcd5043dea00cb8b3f
yonatanayalon <yonatanayalon1@gmail.com>
2017-03-19 00:34:51 +0200

    Add files via upload

---
+++ music-app-frontend/src/app/routing/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { UsersComponent }      from '../users/users.component';
+import { UserComponent }      from '../user/user.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users',     component: UsersComponent },
+  { path: 'users/:id',     component: UserComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
