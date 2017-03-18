f2f58c8890280face57651a112414efae2c3b040
yonatanayalon <yonatanayalon1@gmail.com>
2017-03-19 00:36:26 +0200

    Add files via upload

---
+++ music-app-frontend/src/app/user/user-albums/user-albums.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute }            from '@angular/router';
+
+import { Album }                from '../../models/album';
+import { DataService }         from '../../services/data.service';
+
+@Component({
+  selector: 'user-albums-list',
+  templateUrl: './user-albums.component.html',
+  styleUrls: [ './user-albums.component.css' ]
+})
+export class UserAlbumsComponent implements OnInit {
+  @Input('userId') userId:number;
+  @Output('update') change: EventEmitter<Album> = new EventEmitter<Album>();
+
+  public albums: Album[]
+  private sub: any;
+  private viewModeWide:boolean = false;
+
+  constructor(private dataService: DataService, private route: ActivatedRoute) { }
+
+  getAlbums(): void {
+    this.dataService
+        .getUserAlbums(this.userId)
+        .then(albums => this.albums = albums);
+  }
+
+  toggleView() {
+    this.viewModeWide = !this.viewModeWide;
+  } 
+  
+  removeAlbum(album:Album) {
+    this.dataService.updateUserAlbums(this.userId,album, "remove");
+  }
+
+  ngOnInit(): void {
+      this.getAlbums();
+  }
+
+}
